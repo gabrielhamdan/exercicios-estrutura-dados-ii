a272d0da2c38f20d8095d3c592ae0f08f60e244a
gabrielhamdan <gabriel_hamdan@hotmail.com>
2024-03-09 15:54:11 -0300

    fix: impressão invertida do arquivo

--- semana_1/main.cpp
+++ semana_1/main.cpp
@@ -11,16 +11,19 @@ void count_Aa(char *);
 void count_wrds(char const *);
 long get_file_size(FILE *);
 void print_output();
-void print_reversed_file(char *);
-void process_file_input(FILE *, char *);
+void print_reversed_file();
+void process_file_input(FILE *);
+void push_fcontent(char *);
 char * str_to_lower(char *);
 void update_longest_wrd(char *);
 
-int a_qt = 0;
-int direito_qt = 0;
-int direitos_qt = 0;
-int deveres_qt = 0;
-char longest_wrd[MAX_WRD_LEN] = "";
+int a_qt = 0; // Contador de 'a' (maiúsculos e minúsculos).
+int direito_qt = 0; // Contador de "direito".
+int direitos_qt = 0; // Contador de "direitos".
+int deveres_qt = 0; // Contador de "deveres".
+char longest_wrd[MAX_WRD_LEN] = ""; // Armazena a palavra mais longa do texto.
+char **fcontent; // Array para guardar as linhas lidas do arquivo.
+int fcontent_ct = 0; // Contador de linhas guardadas em fcontent.
 
 int main(int argc, char **argv) {
     FILE *f = fopen("arquivo_texto.txt", "r");
@@ -30,20 +33,18 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    long fsize = get_file_size(f);
-    char *fcontent = (char *)malloc((fsize + 1) * sizeof(char));
+    fcontent = (char **)malloc(sizeof(char *));
     if(fcontent == NULL) {
         printf("Erro ao alocar memória.\n");
         return 1;
     }
 
-    process_file_input(f, fcontent);
+    process_file_input(f);
 
-    print_reversed_file(fcontent);
+    print_reversed_file();
 
     print_output();
 
-    free(fcontent);
     fclose(f);
 
     return 0;
@@ -106,7 +107,7 @@ long get_file_size(FILE *f) {
  * Imprime no console os elementos definidos no enunciado do exerício.
 */
 void print_output() {
-    printf("\n\n");
+    printf("\n-------------------   OUTPUT   -------------------\n");
     printf("A/a: %d\n", a_qt);
     printf("Direito: %d\n", direito_qt);
     printf("Direitos: %d\n", direitos_qt);
@@ -115,11 +116,16 @@ void print_output() {
 }
 
 /**
- * Imprime no console o conteúdo do arquivo de trás para frente.
+ * Imprime, linha a linha, o conteúdo ao contrário do arquivo e, em seguida, libera o seu espaço reservado
+ * na memória. Por último libera o espaço reservado para fcontent.
 */
-void print_reversed_file(char *fcontent) {
-    for(int i = strlen(fcontent) - 1; i >= 0; i--)
-        printf("%c", fcontent[i]);
+void print_reversed_file() {
+    for(int i = fcontent_ct; i >= 0; i--) {
+        printf("%s", fcontent[i]);
+        free(fcontent[i]);
+    }
+
+    free(fcontent);
 }
 
 /**
@@ -127,15 +133,32 @@ void print_reversed_file(char *fcontent) {
  * cada uma das linhas lidas do arquivo para, posteriormente, utilizar o conteúdo da variável em
  * print_reversed_file.
 */
-void process_file_input(FILE *f, char *fcontent) {
+void process_file_input(FILE *f) {
     rewind(f);
     char buff[CHUNK_SIZE];
     while(!feof(f)) {
         fgets(buff, CHUNK_SIZE, f);
+        push_fcontent(buff);
         count_Aa(buff);
         count_wrds(buff);
-        strncat(fcontent, buff, strlen(buff));
+        // strncat(fcontent, buff, strlen(buff));
     }
+}
+
+/**
+ * Reserva espaço na memória e insere em fcontent as linhas do arquivo.
+*/
+void push_fcontent(char *str) {
+    fcontent = (char **)realloc(fcontent, (fcontent_ct + 1) * sizeof(char *));
+    fcontent[fcontent_ct] = (char *)malloc(CHUNK_SIZE);
+
+    if(!fcontent || !fcontent[fcontent_ct]) {
+        printf("Erro ao alocar memória.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    strncpy(fcontent[fcontent_ct], str, CHUNK_SIZE);
+    fcontent_ct++;
 }
 
 /**
